11b8a98b2e85143f7307d5309f002fbabbf00538
Kevin Macfarlane <guitaringkev@gmail.com>
2018-11-09 14:32:54 -0700

    Basic Gyro Implementation
    Added code for moving the player with the gyro. It still needs tweaking I think... It feels a bit janky, but I think that is because I need to reprint the case for it. It'll probably feel better when it is on the spring\

--- DungeonCrawlerSketch/DungeonCrawlerSketch.ino
+++ DungeonCrawlerSketch/DungeonCrawlerSketch.ino
@@ -1,4 +1,5 @@
 #include <FastLED.h>
+#include<Wire.h>
 
 #include "globals.h"
 #include "Player.h"
@@ -11,10 +12,22 @@ int tempspd = 1;
 CRGB board[NUMLEDS];
 #define LED_DATA_PIN 6
 
+//Gyro
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;
+
 
 void setup() {
   // put your setup code here, to run once:
+  //LED Strip
   FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(board, NUMLEDS);
+
+  //Gyroscope
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 }
 
 void loop() {
@@ -24,8 +37,8 @@ void loop() {
   FastLED.clear();
 
   // get the value from the accelerometer here
-
-  player.setSpd(tempspd);
+  getInput();
+  player.setSpd(map(AcX, -20000, 20000, -3, 4));
 
   if ( player.getRightBoundIndex() > NUMLEDS ) {
     player.setAnchorIndex( NUMLEDS - player.getRightBound() );
@@ -46,5 +59,28 @@ void loop() {
 void drawPlayer()
 {
   board[player.getAnchorIndex()] = CRGB(0, 255, 0);
+}
+
+
+void getInput()
+{
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  /*Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);*/
+
+  //Serial.println(map(AcX, -20000, 20000, -3, 4));
 }
 
