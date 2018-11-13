41a67867e75b49c2330dfab0145a9382acc002af
Unknown <devpindroid@gmail.com>
2018-11-13 13:37:21 -0700

    Player Update

--- DungeonCrawlerSketch/player.cpp
+++ DungeonCrawlerSketch/player.cpp
@@ -2,28 +2,38 @@
 
 Player::Player() : Entity( ) {
     // set all of the pixels to white
-    for ( int i = 0; i < PLAYER_LENGTH; ++ i ) {
-        pixels[i].color = 1;
+    for ( int i = 0; i < MAX_PLAYER_LENGTH; ++ i ) {
+        pixels[i].R = 0;
+        pixels[i].G = 255;
+        pixels[i].B = 0;
         pixels[i].index = i;
     }
 
     // set the anchor and boundary point
     anchor = 0;
     left_bound = 0;
-    right_bound = PLAYER_LENGTH;
+    right_bound = MAX_PLAYER_LENGTH;
 };
 
 void Player::updateEntity() {
     anchor += speed;
 
     // update the pixel positions
-    for ( int i = 0; i < PLAYER_LENGTH; ++ i ) {
-        pixels[i].index = anchor + i;
+    for ( int i = 0; i < MAX_PLAYER_LENGTH; ++ i ) {
+        pixels[i].index = getAnchorIndex() + i;
+    }
+
+    if ( attacking ) {
+        ++attacking_counter;
+
+        if ( attacking_counter > ATTACKING_INTERVAL ) {
+            stopAttack();
+        }
     }
 }
 
-void Player::drawEntity() {
-    for ( int i = 0; i < PLAYER_LENGTH; ++ i ) {
-        // draw the pixel to the LED strip
-    }
-}
+//void Player::drawEntity( CRGB board[], const int &blen ) {
+//    for ( int i = 0; i < PLAYER_LENGTH; ++ i ) {
+//        board[ pixels[i].index ] = CRGB( pixels[i].R, pixels[i].G, pixels[i].B );
+//    }
+//}
