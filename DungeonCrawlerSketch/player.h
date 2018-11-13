2f32caf21cd6dd7744b79cf1b0469feb77cb25e5
Unknown <devpindroid@gmail.com>
2018-11-13 13:34:20 -0700

    Player Update
    Sets up it's pixels correctly. Added consts variables for the different states of the player (attacking, roaming) and the change in bounds for these states. Added functions that start and stop attacking, which changes the bounds and colors of the player (single, green LED for roaming, 5 blues LED's for attacking). Also make pixels array public so that it can be updated in main

--- DungeonCrawlerSketch/player.h
+++ DungeonCrawlerSketch/player.h
@@ -3,15 +3,59 @@
 
 #include "Entity.h"
 
-#define PLAYER_LENGTH 5
+// Default values
+#define MAX_PLAYER_LENGTH 5
+#define ROAMING_LEFT_BOUND 0
+#define ROAMING_RIGHT_BOUND 0
+
+// Attacking values
+#define ATTACKING_LEFT_BOUND -2
+#define ATTACKING_RIGHT_BOUND 2
+#define ATTACKING_INTERVAL 180
 
 class Player : public Entity {
 public:
     Player();
     void updateEntity();
-    void drawEntity();
-private:
-    Pixel pixels[PLAYER_LENGTH];
+    //void drawEntity( CRGB board[], const int &blen );
+
+    // maybe we can take these out and just make attacking public
+    bool isAttacking( void ) const { return attacking; };
+
+    void startAttack() {
+        attacking = true;
+        left_bound = ATTACKING_LEFT_BOUND;
+        right_bound = ATTACKING_RIGHT_BOUND;
+
+        for ( int i = 0; i < MAX_PLAYER_LENGTH; ++i ) {
+            pixels[i].R = 0;
+            pixels[i].G = 0;
+            pixels[i].B = 255;
+        }
+    };
+
+    void stopAttack() {
+        attacking = false;
+        left_bound = ROAMING_LEFT_BOUND;
+        right_bound = ROAMING_RIGHT_BOUND;
+
+        for ( int i = 0; i < MAX_PLAYER_LENGTH; ++i ) {
+            pixels[i].R = 0;
+            pixels[i].G = 0;
+            pixels[i].B = 0;
+        }
+
+        pixels[0].G = 255;
+    };
+
+    // appearance
+    Pixel pixels[MAX_PLAYER_LENGTH];
+
+    // actions
+    bool attacking = false;
+
+    // counters
+    int attacking_counter = 0;
 };
 
 
