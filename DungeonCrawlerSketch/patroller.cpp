9f49b338bf69ed38cde92aae8b8562ff75cd7448
Devin Piner <devpindroid@gmail.com>
2018-11-13 22:19:15 -0700

    Initial commit
    The patroller moves back and forth

---
+++ DungeonCrawlerSketch/patroller.cpp
@@ -0,0 +1,33 @@
+#include "patroller.h"
+
+Patroller::Patroller( int spos ) : Entity( ) {
+    // set all of the pixels to white
+    for ( int i = 0; i < MAX_PATROLLER_LENGTH; ++ i ) {
+        pixels[i].R = 0;
+        pixels[i].G = 255;
+        pixels[i].B = 0;
+        pixels[i].index = i;
+    }
+
+    // set the anchor and boundary point
+    starting_position = spos;
+    anchor = spos;
+    left_bound = 0;
+    right_bound = MAX_PATROLLER_LENGTH;
+};
+
+void Patroller::updateEntity() {
+    ++patrol;
+    anchor = starting_position + ( sin( patrol ) * PATROL_SPEED );
+
+    // update the pixel positions
+    for ( int i = 0; i < MAX_PATROLLER_LENGTH; ++ i ) {
+        pixels[i].index = getAnchorIndex() + i;
+    }
+}
+
+void Patroller::drawEntity( CRGB board[], const int &blen ) {
+    for ( int i = 0; i < MAX_PATROLLER_LENGTH; ++ i ) {
+        board[ pixels[i].index ] = CRGB( pixels[i].R, pixels[i].G, pixels[i].B );
+    }
+}
---
+++ DungeonCrawlerSketch/patroller.h
@@ -0,0 +1,31 @@
+#ifndef FINAL_PATROLLER_H
+#define FINAL_PATROLLER_H
+
+#include <FastLED.h>
+#include "Entity.h"
+
+#define MAX_PATROLLER_LENGTH 1
+#define LEFT_BOUND 0
+#define RIGHT_BOUND 0
+
+// patrol pattern
+#define PATROL_SPEED 2
+
+class Patroller : public Entity {
+public:
+    Patroller( int spos );
+    void updateEntity();
+    void drawEntity( CRGB board[], const int &blen );
+
+    // appearance
+    Pixel pixels[MAX_PATROLLER_LENGTH];
+
+    // starting position
+    int starting_position;
+
+    // counters
+    int patrol = 0;
+};
+
+
+#endif //FINAL_PATROLLER_H
