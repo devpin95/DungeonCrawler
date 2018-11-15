195043fc2200cf359d8dcafe2087e7c97b96da17
Devin Piner <devpindroid@gmail.com>
2018-11-15 13:40:43 -0700

    Updates (needs testing)
    Added check for player colliding with left side of board - player gets stuck so that it doesn't keep trying to move when it is at the edge of the board. Added collision check with patroller and lava. If the player dies or the patroller dies, their drawEntity stops drawing any pixels.

--- DungeonCrawlerSketch/DungeonCrawlerSketch.ino
+++ DungeonCrawlerSketch/DungeonCrawlerSketch.ino
@@ -4,10 +4,12 @@
 #include "globals.h"
 #include "Player.h"
 #include "Patroller.h"
+#include "Lava.h"
 
 // Global variables
 Player player;
 Patroller patroller( 150 );
+Lava lava( 75 );
 int tempspd = 1;
 
 //LED Strip 
@@ -20,55 +22,93 @@ int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  //LED Strip
-  FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(board, NUMLEDS);
+    // put your setup code here, to run once:
+    //LED Strip
+    FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(board, NUMLEDS);
 
-  //Gyroscope
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
-  //Serial.begin(9600);
+    //Gyroscope
+    Wire.begin();
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x6B);  // PWR_MGMT_1 register
+    Wire.write(0);     // set to zero (wakes up the MPU-6050)
+    Wire.endTransmission(true);
+    //Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 
-  //clear the board of all game objects
-  FastLED.clear();
+    //clear the board of all game objects
+    FastLED.clear();
 
-  // get the value from the accelerometer here
-  getInput();
-  if (player.isAttacking())//if player is attacking, we don't want him to move
-  {
-    player.setSpd(0);
-  }
-  else
-  {
-    player.setSpd(map(AcX, -20000, 20000, -3, 4));
-  }
+    // get the value from the accelerometer here
+    getInput();
 
-  //check for attack
-  if (abs(GyY) > 20000 || abs(GyZ) > 20000)
-  {
-    player.startAttack();
-  }
-  
-  if ( player.getRightBoundIndex() > NUMLEDS ) {
-    player.setAnchorIndex( NUMLEDS - player.getRightBound() );
-  }
+    if (player.isAttacking())//if player is attacking, we don't want him to move
+    {
+        player.setSpd(0);
+    }
+    else
+    {
+        player.setSpd(map(AcX, -20000, 20000, -3, 4));
+    }
 
-  patroller.updateEntity();
-  player.updateEntity();
+    //check for attack
+    if (abs(GyY) > 20000 || abs(GyZ) > 20000)
+    {
+        player.startAttack();
+    }
 
-  patroller.drawEntity( board, NUMLEDS );
-  player.drawEntity( board, NUMLEDS );
+    // check for collision with the edge of the board
+    if ( player.getRightBoundIndex() > NUMLEDS && player.speed > 0 )
+    {
+        player.stuck_right = true;
+        player.setAnchorIndex( NUMLEDS - player.getRightBound() );
+    }
+    else if ( player.getLeftBoundIndex() < 0 && player.speed < 0 )
+    {
+        player.stuck_left = true;
+        player.setAnchorIndex( 0 + player.getLeftBound() );
+    }
+    else
+        {
+        player.stuck_right = false;
+        player.stuck_left = false;
+    }
 
-  //redraw the game state
-  FastLED.show();
-  delay(FPS);
+    // check for collision with enemies
+    if ( player.getRightBoundIndex() >= patroller.getLeftBoundIndex()
+        && player.getLeftBoundIndex() < patroller.getRightBoundIndex() )
+    {
+        if ( player.attacking ) {
+            patroller.dead = true;
+        } else {
+            player.dead = true;
+        }
+    }
+
+    // check for collision with lava
+    if ( player.getRightBoundIndex() >= lava.getLeftBoundIndex()
+         && player.getLeftBoundIndex() < lava.getRightBoundIndex() )
+    {
+        if ( lava.attacking ) {
+            player.dead = true;
+        }
+    }
+
+    // update all entities
+    lava.updateEntity();
+    patroller.updateEntity();
+    player.updateEntity();
+
+    // draw all entities
+    lava.drawEntity();
+    patroller.drawEntity( board, NUMLEDS );
+    player.drawEntity( board, NUMLEDS );
+
+    //redraw the game state
+    FastLED.show();
+    delay(FPS);
 }
 
 
