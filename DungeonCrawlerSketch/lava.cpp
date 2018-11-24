caee727574c21f57a6aa390e0d2f485f94bee72a
Devin Piner <devpindroid@gmail.com>
2018-11-24 15:49:53 -0700

    Now allows variable length
    the length of the lava can now be set after initialization using setLength(). All lava has an initial length of 0

--- DungeonCrawlerSketch/lava.cpp
+++ DungeonCrawlerSketch/lava.cpp
@@ -1,8 +1,9 @@
 #include "lava.h"
 
-Lava::Lava( int spos ) : Entity( ) {
-    // set all of the pixels to white
-    for ( int i = 0; i < MAX_LAVA_LENGTH; ++ i ) {
+Lava::Lava( const int &spos, ) : Entity( ) {
+    pixels = new Pixel[lava_length];
+
+    for ( int i = 0; i < lava_length; ++ i ) {
         pixels[i].R = 255;
         pixels[i].G = 69;
         pixels[i].B = 0;
@@ -12,8 +13,23 @@ Lava::Lava( int spos ) : Entity( ) {
     // set the anchor and boundary point
     anchor = spos;
     left_bound = 0;
-    right_bound = MAX_LAVA_LENGTH;
+    right_bound = lava_length;
 };
+
+void Lava::setLength( const int &len ) {
+    delete[] pixels;
+    lava_length = len;
+    pixels = new Pixel[lava_length];
+
+    for ( int i = 0; i < lava_length; ++ i ) {
+        pixels[i].R = 255;
+        pixels[i].G = 69;
+        pixels[i].B = 0;
+        pixels[i].index = getAnchorIndex() + i;
+    }
+
+    right_bound = lava_length;
+}
 
 void Lava::updateEntity() {
     ++attacking_counter;
@@ -35,7 +51,7 @@ void Lava::updateEntity() {
     }*/
 }
 void Lava::drawEntity( CRGB board[], const int &blen ) {
-    for ( int i = 0; i < MAX_LAVA_LENGTH; ++ i ) {
+    for ( int i = 0; i < lava_length; ++ i ) {
         board[ pixels[i].index ] = CRGB( pixels[i].R, pixels[i].G, pixels[i].B );
     }
 }
--- DungeonCrawlerSketch/lava.h
+++ DungeonCrawlerSketch/lava.h
@@ -1,7 +1,7 @@
 #ifndef FINAL_LAVA_H
 #define FINAL_LAVA_H
 
-#define MAX_LAVA_LENGTH 10
+//#define MAX_LAVA_LENGTH 10
 
 #define LAVA_ATTACKING_INTERVAL 120
 
@@ -12,13 +12,15 @@
 
 class Lava : public Entity {
 public:
-    Lava( int spos );
+    Lava( const int &spos );
+    ~Lava() { delete[] pixels; };
+    void setLength( const int &len );
     void updateEntity();
     void drawEntity( CRGB board[], const int &blen );
 
     void startAttack() {
         attacking = true;
-        for ( int i = 0; i < MAX_LAVA_LENGTH; ++i ) {
+        for ( int i = 0; i < lava_length; ++i ) {
             pixels[i].R = 255;
             pixels[i].G = 69;
             pixels[i].B = 0;
@@ -27,14 +29,15 @@ public:
 
     void stopAttack() {
         attacking = false;
-        for ( int i = 0; i < MAX_LAVA_LENGTH; ++i ) {
+        for ( int i = 0; i < lava_length; ++i ) {
             pixels[i].R = 30;
             pixels[i].G = 5;
             pixels[i].B = 0;
         }
     };
 
-    Pixel pixels[MAX_LAVA_LENGTH];
+    Pixel *pixels;
+    int lava_length = 0;
 
     // actions
     bool attacking = false;
