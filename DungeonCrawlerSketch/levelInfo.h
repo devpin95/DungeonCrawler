d3602cb4dea9fdab9648585389f2e614153187aa
Devin Piner <devpindroid@gmail.com>
2018-11-24 16:16:25 -0700

    Added new entity variables
    Added enemy delay, patroller offset, and lava length

--- DungeonCrawlerSketch/levelInfo.h
+++ DungeonCrawlerSketch/levelInfo.h
@@ -14,15 +14,15 @@ struct Level
     
     int numEnemies;// = 0;
     int enemyPos[20];// = { 0 };
-    //int enemyDelay[20] = { 0 };
+    int enemyDelay[20] = { 0 };
     
     int numPatrollers;// = 0;
     int patrollerPos[20];// = { 0 };
-    //int patrollerOffset[20] = { 0 };
+    int patrollerOffset[20] = { 0 };
     
     int numLava;// = 0;
     int lavaPos[20];// = { 0 };
-    //int lavaSize[20] = { 0 };
+    int lavaLength[20] = { 0 };
     //int lavaDelay[20] = { 0 };
     
     int numWind;// = 0;
@@ -31,11 +31,29 @@ struct Level
 };
 
 
-Level levels[NUMLEVELS] = 
+Level levels[NUMLEVELS] =
 {
-   { 1,    2, {220, 240},               0, {0},     0, {0},     0, {0}, {0}           },
-   { 1,    4, {160, 180, 220, 240},     0, {0},     0, {0},     0, {0}, {0}           },
-   { 5,    0, {0},                      1, {170},   0, {0},     1, {50}, {1}           }
+    {   // Level 1
+        0,              // player starting position
+        1, {250},       // # of enemies, starting positions
+        0, {0},         // # of patrollers, starting positions
+        0, {0}, {0},    // # of lavas, starting positions, lengths
+        0, {0}          // # of winds, starting positions
+    },
+    {   // Level 2
+        20,
+        1, {250},
+        1, {150},
+        3, {150, 160, 170}, {10, 10, 10},
+        2, {30, 40}
+    },
+    {   // Level 3
+        20,
+        5, {100, 105, 110, 115, 120},
+        3, {150, 155, 160},
+        1, {290}, {30},
+        4, {30, 40, 50, 60}
+    }
 };
 
 
