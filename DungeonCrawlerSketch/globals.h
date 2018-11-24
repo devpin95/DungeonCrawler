92b8d948ece2f3d9ad95e569e1a2d6574af3c92c
Devin Piner <devpindroid@gmail.com>
2018-11-24 15:48:45 -0700

    Added player death animation
    Added variables to draw to the left and right of the player anchor upon death

--- DungeonCrawlerSketch/globals.h
+++ DungeonCrawlerSketch/globals.h
@@ -2,8 +2,13 @@
 #define DUNGEONCRAWLER_GLOBALS_H
 
 // Board
-int const NUMLEDS = 300;
-int const NUMLEVELS = 3;
-int FPS = 1000/60;
+const int NUMLEDS = 300;
+const int NUMLEVELS = 3;
+const int FPS = 1000/60;
+
+//death animation
+int death_left_runner = 0;
+int death_right_runner = 0;
+const int DEATH_FPS = 1000/10;
 
 #endif //DUNGEONCRAWLER_GLOBALS_H
